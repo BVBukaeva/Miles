886c7455d801d6388eabcb7b61cdd1a8b8f94f3f
Bukaeva Bayrta <bbembeeva@gmail.com>
2021-03-21 15:22:59 +0300

    first commit

---
+++ src/miles_calculator.java
@@ -0,0 +1,8 @@
+public class miles_calculator {
+    public static void main(String[] args) {
+        long price=33001;
+        long miles_amount=price/20;
+        System.out.println(miles_amount);
+    }
+}
+
